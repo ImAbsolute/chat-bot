aaf1fcdcfca78d7bb7733e1eb357d11aa4f875f3
Andrey <sereginaa20@yandex.ru>
2021-08-02 20:06:26 +0400

    kostil

--- src/App.js
+++ src/App.js
@@ -7,15 +7,14 @@ import Header from "./components/Header";
 function App({ chatId }) {
 	const [state, setState] = React.useState({
 		messages: {
-			1: { text: "Привет!", sender: "bot" },
-			2: { text: "Здравствуй!", sender: "bot" },
+			1: [{ text: "Привет!", sender: "bot" }],
+			2: [{ text: "Здравствуй!", sender: "bot" }],
 		},
 	});
 	const [chatList, setChatList] = React.useState({
 		chats: {
-			1: { title: "Чат 1", messageList: [state.messages[1]] },
-			2: { title: "Чат 2", messageList: [state.messages[2]] },
-			3: { title: "Чат 3", messageList: [] },
+			1: { title: "Чат 1", messageList: state.messages[1] },
+			2: { title: "Чат 2", messageList: state.messages[2] },
 		},
 	});
 
@@ -23,32 +22,65 @@ function App({ chatId }) {
 		setState({
 			messages: {
 				...state.messages,
-				[chatId]: { text: message, sender: "me" },
+				[chatId]: [...state.messages[chatId], { text: message, sender: "me" }],
 			},
 		});
-		setChatList({
-			chats: {
-				...chatList.chats,
-				[chatId]: {
-					...chatList.chats[chatId],
-					messageList: [...chatList.chats[chatId].messageList, state.messages[chatId]],
-				},
-			},
-		});
+		// setChatList({
+		// 	chats: {
+		// 		...chatList.chats,
+		// 		[chatId]: {
+		// 			...chatList.chats[chatId],
+		// 			messageList: state.messages[chatId],
+		// 		},
+		// 	},
+		// });
 	};
 
-	const handleAddChat = (title) => {
+	const handleAddChat = (title) => {  
 		const chatId = Object.keys(chatList.chats).length + 1;
+		setState({
+			messages: {
+				...state.messages,
+				[chatId]: [],
+			},
+		});
 		setChatList({
 			chats: {
 				...chatList.chats,
-				[chatId]: { title: title, messageList: [] },
+				[chatId]: { title: title, messageList: state.messages[chatId] },
 			},
 		});
 		console.log(chatList);
 	};
+
+  React.useEffect(()=>{
+    setChatList({
+			chats: {
+				...chatList.chats,
+				[chatId]: {
+					...chatList.chats[chatId],
+					messageList: state.messages[chatId],
+				},
+			},
+		});
+    if (state.messages[chatId].[state.messages[chatId].length-1].sender === "me") {
+			setTimeout(
+				() =>
+					setState({
+						messages: {
+              ...state.messages,
+              [chatId]: [...state.messages[chatId], { text: "Привет,я бот", sender: "bot" }],
+            },
+					}),
+				1000
+			);
+		}
+    
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[state])
+
 	console.log(state.messages);
-  console.log(chatList.chats)
+	console.log(chatList.chats);
 	console.log(chatId);
 
 	// React.useEffect(() => {
